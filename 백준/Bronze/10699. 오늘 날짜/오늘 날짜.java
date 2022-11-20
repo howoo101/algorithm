4d27f83a78e7b79b078be8179f7be068ade8bee6
minhowoo <hohohosky101@gmail.com>
2022-11-20 16:21:57 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 18276 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.stream.Collectors.joining;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(new Date()));
+
+
+    }
+
+}
