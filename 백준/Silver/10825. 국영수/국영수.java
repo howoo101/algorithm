905b9e30a73e3853cc1f8b1b656b78c8c5dbb677
minhowoo <hohohosky101@gmail.com>
2022-12-03 17:25:34 +0900

    [Silver IV] Title: 국영수, Time: 812 ms, Memory: 64944 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,18 +1,33 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
-class Student implements Comparable<Student>{
-    String name;
-    int kor;
-    int eng;
-    int math;
+class Student implements Comparable<Student> {
+    private final String name;
+    private final int kor;
+    private final int eng;
+    private final int math;
 
     public Student(String name, int kor, int eng, int math) {
         this.name = name;
         this.kor = kor;
         this.eng = eng;
         this.math = math;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public int getEng() {
+        return eng;
+    }
+
+    public int getMath() {
+        return math;
     }
 
     @Override
@@ -24,13 +39,14 @@ class Student implements Comparable<Student>{
                 ", eng=" + eng +
                 ", math=" + math +
                 "}\n";
+
     }
 
     @Override
     public int compareTo(Student s) {
-        if(this.kor != s.kor)
+        if (this.kor != s.kor)
             return s.kor - this.kor;
-        if(this.eng != s.eng)
+        if (this.eng != s.eng)
             return this.eng - s.eng;
         if (this.math != s.math)
             return s.math - this.math;
@@ -40,26 +56,30 @@ class Student implements Comparable<Student>{
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(bf.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         Student[] answer = new Student[N];
 
         for (int i = 0; i < N; i++) {
-            String[] arr = bf.readLine().split(" ");
-            answer[i] = new Student(arr[0],Integer.parseInt(arr[1]),Integer.parseInt(arr[2]),Integer.parseInt(arr[3]));
+            String[] arr = br.readLine().split(" ");
+            answer[i] = new Student(arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2]), Integer.parseInt(arr[3]));
         }
-        Arrays.stream(answer).sorted().map(student -> student.name).forEach(s -> {
-            try {
-                bw.write(s);
-                bw.newLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        Arrays.stream(answer).sorted()
+                .map(Student::getName)
+                .forEach(s -> {
+                    try {
+                        bw.write(s);
+                        bw.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
         bw.flush();
+        bw.close();
+        br.close();
     }
 
 }
