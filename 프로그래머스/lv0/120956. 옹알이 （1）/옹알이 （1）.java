100997925a2706da9dbcda06c9cf1b56eaccda7b
minhowoo <hohohosky101@gmail.com>
2022-12-22 01:20:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        String[] pronunciations = {"aya", "ye", "woo", "ma"};
+        for (String bab : babbling) {
+            int cnt = 0;
+            for (String pronunciation : pronunciations) {
+                if (bab.contains(pronunciation)) {
+                    cnt += pronunciation.length();
+                    if (cnt == bab.length()) {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+
+        }
+        return answer;
+    }
+}
