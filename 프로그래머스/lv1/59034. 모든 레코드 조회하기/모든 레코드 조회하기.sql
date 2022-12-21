8a7da2a8e890e7ea5ad0df82db291df56e0ba360
minhowoo <hohohosky101@gmail.com>
2022-12-21 22:39:54 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+-- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+-- 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+SELECT * FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
