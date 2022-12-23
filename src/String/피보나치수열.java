494e4f1732ca17e77f98d834ae04670b3f8f1ee8
mino <hohohosky101@gmail.com>
2022-12-23 15:50:11 +0900

    [Array(1, 2차원 배열)]피보나치 수열

---
+++ src/String/피보나치수열.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 피보나치수열 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input1 = in.nextInt();
+        System.out.println(Arrays.toString(new 피보나치수열().solution(input1)).replaceAll("[^0-9,]", "")
+                .replace(",", " "));
+        for(int x :new 피보나치수열().solution(input1))
+            System.out.print(x + " ");
+    }
+
+    private int[] solution(int input1) {
+        int[] answer = new int[input1];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < input1; i++) {
+            answer[i] = answer[i - 2] + answer[i - 1];
+        }
+        return answer;
+    }
+}
