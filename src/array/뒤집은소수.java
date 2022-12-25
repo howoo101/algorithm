093fd5ea62a73cc4b665e8847537fb7b34a38a4d
mino <hohohosky101@gmail.com>
2022-12-26 01:02:23 +0900

    [Array(1, 2차원 배열)]뒤집은 소수

--- src/array/뒤집은소수.java
+++ src/array/뒤집은소수.java
@@ -18,39 +18,29 @@ public class 뒤집은소수 {
 
     private int solution(int i1) {
         int reverseNumber = getReverseNumber(i1);
-
-        return getPrimeNumber(reverseNumber);
-
-
+        if(isPrimeNumber(reverseNumber))
+            return reverseNumber;
+        return 1;
     }
 
     // 숫자 뒤집는 함수
     private int getReverseNumber(int number) {
-
         int reverseNumber = 0;
 
         do {
-            reverseNumber *= 10;
-            reverseNumber += number % 10;
+            reverseNumber = reverseNumber * 10 +  number % 10;
         } while ((number /= 10) != 0);
 //        System.out.println("reverseNumber = " + reverseNumber);
         return reverseNumber;
     }
 
-    private int getPrimeNumber(int N) {
-        int[] arr = new int[N + 1];
-        if (N < 2) {
-            arr[0] = 1;
-            arr[1] = 1;
-        }
+    private boolean isPrimeNumber(int N) {
+        if (N < 2) return false;
 
-        for (int i = 2; i <= N; i++) {
-            if (arr[i] == 0) {
-                for (int j = i; j <= N; j += i) if (i != j) arr[j] = 1;
-            }
+        for (int i = 2; i < N; i++) {
+            if (N%i == 0) return false;
         }
-
-        return arr[N] == 1 ? 1 : N;
+        return true;
     }
 
 
