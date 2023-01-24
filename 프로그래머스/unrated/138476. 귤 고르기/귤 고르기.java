23d6d5be3256650d4cb992872a12c78cc7921993
minhowoo <hohohosky101@gmail.com>
2023-01-24 21:40:53 +0900

    [unrated] Title: 귤 고르기, Time: 38.08 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public int solution(int k, int[] tangerine) {
+        int answer = 1;
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int key : tangerine) {
+            map.merge(key, 1, Integer::sum);
+        }
+
+        Integer[] values = map.values().toArray(Integer[]::new);
+        Arrays.sort(values);
+        int sum = 0;
+        int count = 0;
+        for (int i = values.length - 1; i >= 0; i--,count++) {
+            sum += values[i];
+            if(sum >= k) {
+                return count + 1;
+            }
+        }
+        return answer;
+    }
+}
