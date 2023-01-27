a0b804b217c86b006675b54f0a8a95578559a8f7
minhowoo <hohohosky101@gmail.com>
2023-01-27 16:40:14 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
@@ -1,3 +1,11 @@
 def solution(n, k):
+#     10인분당 음료 하나
+# 12000 / 2000
+    # n = int(n)
+    # k = int(k)
+    k -= n//10
+    
     answer = 0
+    answer += (k*2000) + (12000*n)
+    
     return answer
