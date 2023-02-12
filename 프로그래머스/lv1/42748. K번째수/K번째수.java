7264e13a7eb718fee0b4f637c488254b466de15b
minhowoo <hohohosky101@gmail.com>
2023-02-12 15:29:41 +0900

    [level 1] Title: K번째수, Time: 1.76 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,26 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        //일단 commands 돌면서 i j k를 얻어야한다.
-        //그다음 i j만큼 for문 돌아서 배열 얻어내고 
-        //정렬한다음에 k번째 얻어내기 
+        int[] answer = new int[commands.length];
+        //array 기준으로 start부터 end까지 자르고
         
-        int start = 0 ;
-        int end = 0;
-        int seq = 0;
-        int[] b = new int[commands.length];
         for(int i = 0; i < commands.length; i++) {
-            start = commands[i][0];
-            end = commands[i][1];
-            seq = commands[i][2];
-            int[] a = new int[end-start+1];
-            for(int j = start-1; j < end; j++) {
-                a[j-start+1]=array[j];
-            }
-            java.util.Arrays.sort(a);
-            b[i] = a[seq-1];   
+            int[] tmp = commands[i];
+            int start = tmp[0];    //-> i
+            int end = tmp[1];   //-> j   
+            int idx = tmp[2];   //-> k (요넘이 정답)   
+            int[] slice =  Arrays.stream(array, start-1, end)
+                                 .toArray();
+            Arrays.sort(slice);
+            answer[i] = slice[idx-1];
         }
-        answer = b;
+        
         return answer;
     }
 }
