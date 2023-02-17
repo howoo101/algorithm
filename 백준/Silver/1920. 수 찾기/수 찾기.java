f8fb3bf28ed613ef2f0cfff1f22b60700bdb9665
minhowoo <hohohosky101@gmail.com>
2023-02-17 21:13:04 +0900

    [Silver IV] Title: 수 찾기, Time: 1376 ms, Memory: 59656 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,25 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
         List<Integer> list = new ArrayList<>();
-        while (st2.hasMoreTokens()) {
-            list.add(Integer.parseInt(st2.nextToken()));
+        while (st1.hasMoreTokens()) {
+            list.add(Integer.parseInt(st1.nextToken()));
         }
         Collections.sort(list);
-        StringTokenizer st3 = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st3.nextToken());
+        int M = Integer.parseInt(br.readLine());
         List<Integer> list2 = new ArrayList<>();
-        StringTokenizer st4 = new StringTokenizer(br.readLine());
-        while (st4.hasMoreTokens()) {
-            list2.add(Integer.parseInt(st4.nextToken()));
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        while (st2.hasMoreTokens()) {
+            list2.add(Integer.parseInt(st2.nextToken()));
         }
+
         for (Integer integer : list2) {
             int idx = Collections.binarySearch(list, integer);
             System.out.println(idx >=0 ? 1 :0);
